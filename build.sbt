fea19226436542e0b60310b5e5037900ee317581
Alexander Daniel <alexander.daniel@gmx.at>
2013-01-29 16:05:10 +0100

    Upgrade to Scala 2.10 and ScalaTest 1.9

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "plus-minus"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.9.2"
+scalaVersion := "2.10.0"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.2" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
 
--- src/main/scala/lachdrache/PlusMinusParser.scala
+++ src/main/scala/lachdrache/PlusMinusParser.scala
@@ -20,6 +20,7 @@ class PlusMinusParser extends JavaTokenParsers {
 
   def apply(input :String):Int = parseAll(expr, input) match {
     case Success(result, _) => result
+    case NoSuccess(msg, _) => throw new RuntimeException(msg)
     case Failure(msg, _) => throw new RuntimeException(msg)
   }
 }
